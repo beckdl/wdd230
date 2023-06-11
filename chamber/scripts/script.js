355f036995a8941b3850f5c80d9b8829af232cc9
beckdl <icefire24@hotmail.com>
2023-06-10 22:31:18 -0600

    added join page with additional thank you page.

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -38,4 +38,18 @@ if (currentDay === 1 || currentDay === 2) {
     banner.style.display = "block";
 };
 
-// weather app
+// form
+
+const formDate = document.querySelector(".formDate");
+// For todays date;
+Date.prototype.today = function () { 
+    return ((this.getDate() < 10)?"0":"") + this.getDate() +"/"+(((this.getMonth()+1) < 10)?"0":"") + (this.getMonth()+1) +"/"+ this.getFullYear();
+}
+
+// For the time now
+Date.prototype.timeNow = function(){     
+    return ((this.getHours() < 10)?"0":"") + ((this.getHours()>12)?(this.getHours()-12):this.getHours()) +":"+ ((this.getMinutes() < 10)?"0":"") + this.getMinutes() + ((this.getHours()>12)?(' PM'):' AM'); 
+};
+var formTime = new Date().today() + " " + new Date().timeNow();
+formDate.textContent = formTime;
+

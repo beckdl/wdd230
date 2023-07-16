37b7ae92e06dcff04706bdedfdcd976b56519f24
beckdl <icefire24@hotmail.com>
2023-07-16 17:43:45 -0600

    commit

--- bountifulFoods/scripts/fruits.js
+++ bountifulFoods/scripts/fruits.js
@@ -1,12 +1,14 @@
 const urlFood ="https://beckdl.github.io/wdd230/bountifulFoods/json/fruit.json";
 
+
+
 async function apiFetch() {
     try {
         const response = await fetch(urlFood);
         if (response.ok) {
             const data = await response.json();
-            console.log(data.fruits); // this is for testing the call
-            displayResults(data.fruits);
+            console.log(data); // this is for testing the call
+            displayResults1(data.fruits);
         } else {
             throw Error(await response.text());
         }
@@ -19,19 +21,21 @@ apiFetch();
 
 const displayResults = (fruits) => {
     const options = document.getElementById('fruits');
-    fruits.forEach((fruit) => {
+
+    fruits.forEach((num) => {
         // Create elements to add to the div.cards element
-        let option = document.createElement('section');
+        let option = document.createElement('option');
         
   
         // Build the h2 content out to show the prophet's full name - finish the template string
-        
+        option.textContent = `${num.name}`;
 
         // Build the image portrait by setting all the relevant attribute
-        option.setAttribute('value', fruit.name)
+        option.setAttribute('value', num.name);
   
         // Append the section(card) with the created elements
         options.appendChild(option);
         
     })
-}
+};
+
